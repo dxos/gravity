86506f671774cac42e999cadd19bde46f24f3c19
Dmytro Maretskyi <maretskii@gmail.com>
2020-08-11 12:17:24 +0300

    Fix storybook

--- packages/testbench/.storybook/main.js
+++ packages/testbench/.storybook/main.js
@@ -1,8 +1,25 @@
 //
 // Copyright 2020 DXOS.org
 //
+const path = require('path');
 
 module.exports = {
   stories: ['../stories/*.[tj]sx'],
-  addons: ['@storybook/preset-typescript', '@storybook/addon-actions', '@storybook/addon-links']
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('awesome-typescript-loader'),
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
 };
