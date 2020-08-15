321cf803b4c1d12db98fe988fc10ca2daf14b5d7
David Boreham <david@wireline.io>
2020-08-15 00:13:36 -0600

    Private members

--- packages/feed-test-network/src/feed-node.test.js
+++ packages/feed-test-network/src/feed-node.test.js
@@ -22,7 +22,7 @@ const randomMessage = () => {
 const TestAgentFactory = async (topic, peerId) => {
   const agent = new TestAgent();
   await agent.initialize(topic);
-  const node = new FeedNode(agent.feedStore, agent.feed);
+  const node = new FeedNode(agent._feedStore, agent._feed);
   await node.initialize(topic, peerId);
   node.agent = agent;
   return node;
--- packages/feed-test-network/src/testing/test-agent.js
+++ packages/feed-test-network/src/testing/test-agent.js
@@ -11,22 +11,25 @@ import ram from 'random-access-memory';
 const log = debug('dxos:feed-store-network');
 
 export class TestAgent {
-  feedStore;
-  feed;
+  /** @type {FeedStore} */
+  _feedStore;
+
+  /** @type {Feed} */
+  _feed;
 
   async initialize (topic) {
     // TODO(dboreham): Allow specification of storage type and encoding.
-    this.feedStore = await FeedStore.create(ram, { feedOptions: { valueEncoding: 'json' } });
-    this.feed = await this.feedStore.openFeed('/feed', { metadata: { topic: topic.toString('hex') } });
+    this._feedStore = await FeedStore.create(ram, { feedOptions: { valueEncoding: 'json' } });
+    this._feed = await this._feedStore.openFeed('/feed', { metadata: { topic: topic.toString('hex') } });
   }
 
   async append (msg) {
-    return this.feed.append(msg);
+    return this._feed.append(msg);
   }
 
   async getMessages () {
     const messages = [];
-    const stream = this.feedStore.createReadStream();
+    const stream = this._feedStore.createReadStream();
     stream.on('data', (data) => {
       log(`Subscription message: ${JSON.stringify(data)}`);
       messages.push(data.data);
