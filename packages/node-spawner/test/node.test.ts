a4947b17d48021ec2e468fe4dd0be5cba75efe6e
Dmytro Maretskyi <maretskii@gmail.com>
2020-08-31 16:00:28 +0300

    Wait for event

--- packages/node-spawner/test/node.test.ts
+++ packages/node-spawner/test/node.test.ts
@@ -1,14 +1,18 @@
 import { NodeOrchestrator } from '../src/node-orchestrator';
 import { Platform } from '../src/node-factory';
+import { sleep } from '@dxos/async';
 
 test('fork TestAgent', async () => {
   const orchestrator = new NodeOrchestrator();
 
   const node = await orchestrator.createNode(require.resolve('./client-agent'), Platform.NODE);
+  node.metrics.update.on(() => console.log(node.metrics.asObject()));
 
   node.sendEvent({});
 
   node.snapshot();
+
+  await node.metrics.update.waitFor(() => node.metrics.getNumber('updated') === 1)
 
   orchestrator.destroy();
 });
